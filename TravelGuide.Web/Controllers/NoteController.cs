d2a5890170f575ddf6e02f362665987e9d268ea9
Onur Güdül <onurgudul229@gmail.com>
2020-12-16 23:57:24 +0300

    Note Controller

--- TravelGuide.Web/Controllers/NoteController.cs
+++ TravelGuide.Web/Controllers/NoteController.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TravelGuide.Business.Abstract;
+using TravelGuide.Entities.Concreate;
 
 namespace TravelGuide.Web.Controllers
 {
     public class NoteController : Controller
     {
         private readonly INoteService _noteService;
-        public NoteController(INoteService noteService)
+        private readonly ICategoryService _categoryService;
+        public NoteController(INoteService noteService, ICategoryService categoryService)
         {
             _noteService = noteService;
+            _categoryService = categoryService;
         }
         // GET: Note
         public ActionResult Index()
@@ -23,7 +27,7 @@ namespace TravelGuide.Web.Controllers
         }
         public ActionResult Details(int? id)
         {
-            if (id==null)
+            if (id == null)
             {
                 return HttpNotFound();
             }
@@ -33,6 +37,38 @@ namespace TravelGuide.Web.Controllers
                 return HttpNotFound();
             }
             return View(notes.Data);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var result = _noteService.GetById(id.Value);
+            if (!result.Success)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ViewBag.CategoryId = new SelectList(_categoryService.GetList().Data, "Id", "Title", result.Data.CategoryId);
+            return View(result.Data);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Note note)
+        {
+            var notes = _noteService.GetById(note.Id);
+            if (!notes.Success)
+            {
+                return HttpNotFound();
+            }
+            notes.Data.IsDraft = note.IsDraft;
+            notes.Data.CategoryId = note.CategoryId;
+            notes.Data.Text = note.Text;
+            notes.Data.Title = note.Text;
+            _noteService.Update(notes.Data);
+            ViewBag.CategoryId = new SelectList(_categoryService.GetList().Data, "Id", "Title", notes.Data.CategoryId);
+            return RedirectToAction("Index");
         }
     }
 }
