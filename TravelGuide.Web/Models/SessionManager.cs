3b82b7b2f6eee4186c50783b02dc110a587ec3dd
Onur Güdül <onurgudul229@gmail.com>
2020-12-17 20:09:36 +0300

    session manager change

---
+++ TravelGuide.Web/Models/SessionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TravelGuide.Entities.Concreate;
+
+namespace TravelGuide.Web.Models
+{
+    public class SessionManager
+    {
+        public static User User
+        {
+            get
+            {
+                return Get<User>("login");
+            }
+        }
+        public static void Set<T>(string key, T obj)
+        {
+            HttpContext.Current.Session[key] = obj;
+        }
+        public static T Get<T>(string key)
+        {
+            if (HttpContext.Current.Session[key] != null)
+            {
+                return (T)HttpContext.Current.Session[key];
+            }
+            return default(T);
+        }
+        public static void Remove(string key)
+        {
+            if (HttpContext.Current.Session[key] != null)
+            {
+                HttpContext.Current.Session.Remove(key);
+            }
+        }
+        public static void Clear()
+        {
+            HttpContext.Current.Session.Clear();
+        }
+    }
+}
